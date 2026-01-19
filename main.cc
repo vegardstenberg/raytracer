d0d379117e62d9690d0aea9a69f57340035c799e
Vegard Stenberg <vegard.h.stenberg@gmail.com>
2026-01-19 16:06:43 +0100

    section3

--- main.cc
+++ main.cc
@@ -1,5 +1,8 @@
 #include <iostream>
 
+#include "vec3.h"
+#include "color.h"
+
 int main() {
   int img_width = 255;
   int img_height = 255;
@@ -7,18 +10,14 @@ int main() {
   std::cout << "P3\n" << img_width << ' ' << img_height << "\n255\n";
 
   for (int j = 0; j < img_height; j++) {
+    std::clog << "\rScanlines remaining: " << (img_height - j) << ' ' << std::flush;
     for (int i = 0; i < img_width; i++) {
-      auto r = double(i) / (img_width-1);
-      auto g = double(j) / (img_height-1);
-      auto b = 0.0;
-
-      int ir = int(255.999 * r);
-      int ig = int(255.999 * g);
-      int ib = int(255.999 * b);
-    
-      std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+      auto pixel_color = color(double(i)/(img_width-1), double(j)/(img_height-1), 0);
+      write_color(std::cout, pixel_color);
     }
   }
+
+  std::clog << "Done           \n"; 
 
   return 0;
 }
