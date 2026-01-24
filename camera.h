0588c6d215683aff973dfb74e572ca45fbe375d1
Vegard Stenberg <vegard.h.stenberg@gmail.com>
2026-01-24 13:28:51 +0100

    section 9

--- camera.h
+++ camera.h
@@ -8,6 +8,8 @@ class camera {
     double aspect_ratio = 1.0;  // Ratio of image width over height
     int    image_width  = 100;  // Rendered image width in pixel count
     double samples_per_pixel = 10;
+    int max_depth = 10;
+      
 
     void render(const hittable& world) {
         initialize();
@@ -20,7 +22,7 @@ class camera {
               color pixel_color(0, 0, 0);
               for (int sample = 0; sample < samples_per_pixel; sample++) {
                 ray r = get_ray(i, j);
-                pixel_color += ray_color(r, world);
+                pixel_color += ray_color(r, max_depth, world);
               }
               write_color(std::cout, pixel_samples_scale * pixel_color);
             }
@@ -78,11 +80,15 @@ class camera {
     }
 
 
-    color ray_color(const ray& r, const hittable& world) const {
+    color ray_color(const ray& r, int depth, const hittable& world) const {
+      if (depth <= 0) // to limit the recursion depth
+        return color(0, 0, 0);
+
       hit_record rec;
 
-      if (world.hit(r, interval(0, infinity), rec)) {
-        return 0.5 * (rec.normal + color(1,1,1));
+      if (world.hit(r, interval(0.001, infinity), rec)) {
+        vec3 direction = rec.normal + random_unit_vector();
+        return 0.5 * ray_color(ray(rec.p, direction), depth-1, world);
       }
 
       vec3 unit_direction = unit_vector(r.direction());
