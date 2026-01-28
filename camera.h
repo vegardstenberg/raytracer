92c9fae9ca99cc7fc54c053eb6996e5ed8b7659e
Vegard Stenberg <vegard.h.stenberg@gmail.com>
2026-01-28 12:22:29 +0100

    finished book 1

--- camera.h
+++ camera.h
@@ -2,6 +2,7 @@
 #define CAMERA_H
 
 #include "hittable.h"
+#include "material.h"
 
 class camera {
   public:
@@ -9,7 +10,14 @@ class camera {
     int    image_width  = 100;  // Rendered image width in pixel count
     double samples_per_pixel = 10;
     int max_depth = 10;
-      
+
+    double vfov = 90;
+    point3 lookfrom = point3(0,0,0);
+    point3 lookat = point3(0, 0, -1);
+    vec3 vup = vec3(0,1,0);
+
+    double defocus_angle = 0;
+    double focus_dist = 10;
 
     void render(const hittable& world) {
         initialize();
@@ -37,6 +45,9 @@ class camera {
     point3 pixel00_loc;    // Location of pixel 0, 0
     vec3   pixel_delta_u;  // Offset to pixel to the right
     vec3   pixel_delta_v;  // Offset to pixel below
+    vec3 u, v, w;
+    vec3 defocus_disk_u;
+    vec3 defocus_disk_v;
 
     void initialize() {
       image_height = int(image_width / aspect_ratio);
@@ -44,16 +55,21 @@ class camera {
 
       pixel_samples_scale = 1.0 / samples_per_pixel;
 
-      center = point3(0, 0, 0);
+      center = lookfrom;
 
       // Determine viewport dimensions.
-      auto focal_length = 1.0;
-      auto viewport_height = 2.0;
+      auto theta = degrees_to_radians(vfov);
+      auto h = std::tan(theta/2);
+      auto viewport_height = 2.0 * h * focus_dist;
       auto viewport_width = viewport_height * (double(image_width)/image_height);
 
+      w = unit_vector(lookfrom - lookat);
+      u = unit_vector(cross(vup, w));
+      v = unit_vector(cross(w, u));
+
       // Calculate the vectors across the horizontal and down the vertical viewport edges.
-      auto viewport_u = vec3(viewport_width, 0, 0);
-      auto viewport_v = vec3(0, -viewport_height, 0);
+      vec3 viewport_u = viewport_width * u;
+      vec3 viewport_v = viewport_height * -v;
 
       // Calculate the horizontal and vertical delta vectors from pixel to pixel.
       pixel_delta_u = viewport_u / image_width;
@@ -61,15 +77,19 @@ class camera {
 
       // Calculate the location of the upper left pixel.
       auto viewport_upper_left =
-          center - vec3(0, 0, focal_length) - viewport_u/2 - viewport_v/2;
+          center - focus_dist * w - viewport_u/2 - viewport_v/2;
       pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+
+      auto defocus_radius = focus_dist * std::tan(degrees_to_radians(defocus_angle / 2));
+      defocus_disk_u = u * defocus_radius;
+      defocus_disk_v = v * defocus_radius;
     }
   
     ray get_ray(int i, int j) const {
       auto offset = sample_square();
       auto pixel_sample = pixel00_loc + ((i + offset.x()) * pixel_delta_u)
                             + ((j + offset.y()) * pixel_delta_v);
-      auto ray_origin = center;
+      auto ray_origin = (defocus_angle <= 0) ? center : defocus_disk_sample();
       auto ray_direction = pixel_sample - ray_origin;
 
       return ray(ray_origin, ray_direction);
@@ -78,7 +98,11 @@ class camera {
     vec3 sample_square() const {
       return vec3(random_double()-0.5, random_double()-0.5, 0);
     }
-
+    
+    point3 defocus_disk_sample() const {
+      auto p = random_in_unit_disk();
+      return center + (p[0] * defocus_disk_u) + (p[1] * defocus_disk_v);
+    }
 
     color ray_color(const ray& r, int depth, const hittable& world) const {
       if (depth <= 0) // to limit the recursion depth
@@ -87,8 +111,11 @@ class camera {
       hit_record rec;
 
       if (world.hit(r, interval(0.001, infinity), rec)) {
-        vec3 direction = rec.normal + random_unit_vector();
-        return 0.5 * ray_color(ray(rec.p, direction), depth-1, world);
+        ray scattered;
+        color attenuation;
+        if (rec.mat->scatter(r, rec, attenuation, scattered))
+          return attenuation * ray_color(scattered, depth-1, world);
+        return color(0,0,0);
       }
 
       vec3 unit_direction = unit_vector(r.direction());
--- vec3.h
+++ vec3.h
@@ -41,6 +41,11 @@ class vec3 {
     double length_squared() const {
       return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
     }
+
+    bool near_zero() const {
+      auto s = 1e-8;
+      return (std::fabs(e[0] < s) && (std::fabs(e[1] <s)) && std::fabs(e[2] < s));
+    }
     
     static vec3 random() {
       return vec3(random_double(), random_double(), random_double());
@@ -114,8 +119,27 @@ inline vec3 random_on_hemisphere(const vec3& normal) {
     return -on_unit_sphere;
 }
 
+inline vec3 reflect(const vec3 v, const vec3& n) {
+  return v - 2*dot(v,n)*n;
+}
+
+inline vec3 refract(const vec3& uv, const vec3& n, double etai_over_etat) {
+  auto cos_theta = std::fmin(dot(-uv, n), 1.0);
+  vec3 r_out_perp = etai_over_etat * (uv + cos_theta*n);
+  vec3 r_out_parallel = -std::sqrt(std::fabs(1.0 - r_out_perp.length_squared())) * n;
+  return r_out_perp + r_out_parallel;
+}
+
 inline vec3 unit_vector (const vec3& v) {
   return v / v.length();
+}
+
+inline vec3 random_in_unit_disk() {
+  while (true) {
+    auto p = vec3(random_double(-1, 1), random_double(-1, 1), 0);
+    if (p.length_squared() < 1)
+      return p;
+  }
 }
 
 #endif
